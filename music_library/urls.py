71748e272f3d2769b5d8e86f0f2937e471c464dc
LizzieDworsky <amydworsky15@gmail.com>
2022-06-17 15:41:27 -0600

    updated url + added likes to Song model

--- music_library/urls.py
+++ music_library/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('music/', include("songs.urls")),
+    path('song/', include("songs.urls")),
 ]
