cf04a26606a03bcc8e5018b78d7eacd7c7aa7b02
LizzieDworsky <amydworsky15@gmail.com>
2022-06-17 13:56:31 -0600

    connected songs app to admin site to seed data to the database + created and tested (using Postman) the get all songs and get song by pk

--- songs/views.py
+++ songs/views.py
@@ -1,13 +1,26 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import SongSerializer
+from .models import Song
 
 # Create your views here.
 
 @api_view(["GET"])
 def songs_list(request):
-    return Response("Hello World")
+    
+    if request.method == "GET":
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)
 
 
 @api_view(["GET"])
-def song_details(request):
-    return Response("Hello World Details")
+def song_details(request, pk):
+    song = get_object_or_404(Song, pk=pk)
+    if request.method == "GET":
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
