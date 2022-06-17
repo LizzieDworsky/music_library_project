62c2ffb1d9ad99e44efedc0784c9bea7cc96c126
LizzieDworsky <amydworsky15@gmail.com>
2022-06-17 12:50:41 -0600

    added url paths + simple views functions for testing + created the serializer for Song model

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+@api_view(["GET"])
+def songs_list(request):
+    return Response("Hello World")
+
+
+@api_view(["GET"])
+def song_details(request):
+    return Response("Hello World Details")
