f20e296b297f4468583a0750ab16c6b9e75bd78e
LizzieDworsky <amydworsky15@gmail.com>
2022-06-17 12:25:02 -0600

    made Song model in songs app

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.forms import CharField, DateField
 
 # Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=255)
